003a801ef0b9b6afb153149d722186cfb1eaca6e
Slava <slava.milin@gmail.com>
2017-08-20 19:18:30 +0300

    fix remarks

--- js/setup.js
+++ js/setup.js
@@ -3,15 +3,14 @@
 var userDialog = document.querySelector('.setup');
 var personsBlock = document.querySelector('.setup-similar');
 var similarList = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 var wizzards = {
   firstNames: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
   secondNames: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
   coatColors: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
   eyesColors: ['black', 'red', 'blue', 'yellow', 'green'],
-  name: '.setup-similar-label',
-  coat: '.wizard-coat',
-  eyes: '.wizard-eyes',
+  nameSelecter: '.setup-similar-label',
+  coatSelecter: '.wizard-coat',
+  eyesSelecter: '.wizard-eyes',
 };
 var selectAttribute = function (attribute) {
   return attribute[Math.round(Math.random() * (attribute.length - 1))];
@@ -19,22 +18,27 @@ var selectAttribute = function (attribute) {
 var removeClass = function (element, name) {
   element.classList.remove(name);
 };
-var setColor = function (node, attributePlace, attributeValue, index) {
-  node.querySelectorAll(attributePlace)[index].setAttribute('style', 'fill: ' + attributeValue);
+var setColor = function (node, attributePlace, attributeValue) {
+  node.querySelector(attributePlace).setAttribute('style', 'fill: ' + attributeValue);
 };
-var setName = function (node, attributePlace, attributeValue, index) {
-  node.querySelectorAll(attributePlace)[index].insertAdjacentHTML('afterbegin', '<span>' + attributeValue + ' </span>');
+var setName = function (node, attributePlace, attributeValue) {
+  node.querySelector(attributePlace).insertAdjacentText('afterbegin', attributeValue + ' ');
 };
-var cloneElements = function () {
+var renderWizardsTo = function (list) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
   for (var i = 0; i < 4; i += 1) {
     var cloneTemplate = similarWizardTemplate.cloneNode(true);
-    similarList.appendChild(cloneTemplate);
-    setColor(similarList, wizzards.coat, selectAttribute(wizzards.coatColors), i);
-    setColor(similarList, wizzards.eyes, selectAttribute(wizzards.eyesColors), i);
-    setName(similarList, wizzards.name, selectAttribute(wizzards.secondNames), i);
-    setName(similarList, wizzards.name, selectAttribute(wizzards.firstNames), i);
+    var randomCoatColor = selectAttribute(wizzards.coatColors);
+    var randomEyesColor = selectAttribute(wizzards.eyesColors);
+    var randomFirstName = selectAttribute(wizzards.firstNames);
+    var randomSecontName = selectAttribute(wizzards.secondNames);
+    setColor(cloneTemplate, wizzards.coatSelecter, randomCoatColor);
+    setColor(cloneTemplate, wizzards.eyesSelecter, randomEyesColor);
+    setName(cloneTemplate, wizzards.nameSelecter, randomSecontName);
+    setName(cloneTemplate, wizzards.nameSelecter, randomFirstName);
+    list.appendChild(cloneTemplate);
   }
 };
 removeClass(userDialog, 'hidden');
 removeClass(personsBlock, 'hidden');
-cloneElements();
+renderWizardsTo(similarList);
